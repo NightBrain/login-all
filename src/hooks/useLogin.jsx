4cf068de070be6dda83c4dd5569bb430ae2e52c7
NightBrain <dumexnightbrain@gmail.com>
2023-08-21 23:22:05 +0700

    commit03

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../config";
 import { AuthContext } from "../context/AuthContext";
+import { createUserDocment } from "../createUserDocument";
 // Github Login
 export const useLogin = () => {
   const [error, setError] = useState(false);
@@ -15,13 +16,13 @@ export const useLogin = () => {
     setISPending(true);
 
     try {
-
       const res = await signInWithPopup(auth, provider);
       if (!res) {
         throw new Error("Could not complete signup");
       }
 
       const user = res.user;
+      await createUserDocment(user);
       dispatch({type: "LOGIN", payload: user})
       console.log(user);
       setISPending(false);
